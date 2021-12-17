4b38a4989e659eaca705478142272dd5231bcc74
Oluwafemi-iDEAR <ifesanmioluwafemi@gmail.com>
2021-12-17 12:42:55 +0100

    initial commit

---
+++ vendors/shop/shop.js
@@ -0,0 +1,12 @@
+const shop = (function () {
+
+    const showItems = () => {
+        // console.log('showing Items');
+        //get items from localstorage
+
+    }
+
+    return {
+        showItems: showItems
+    }
+}());
